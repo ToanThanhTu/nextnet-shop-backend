630f2d68e1e7a7fc782b458119b3acee82fa2ab2
toanthanhtu <toantu3005@gmail.com>
2024-12-07 14:51:38 +1100

    first commit

---
+++ Item.cs
@@ -0,0 +1,8 @@
+public class Item
+{
+  public int Id { get; set; }
+  public string? Name { get; set; }
+  public double Price { get; set; }
+  public bool IsSold { get; set; }
+  public string? Secret { get; set; }
+}
