b4ddffabfcaf7e9e51f563216f119cdd2a1e3c52
toanthanhtu <toantu3005@gmail.com>
2024-12-20 15:43:33 +1100

    fix data models

--- Data/Types/Category.cs
+++ Data/Types/Category.cs
@@ -1,7 +1,7 @@
 public class Category
 {
-  public required string Id { get; set; }
+  public int Id { get; set; }
   public required string Title { get; set; }
   public string? Description { get; set; }
-  public List<SubCategory> SubCategories { get; set; } = [];
+  public List<SubCategory>? SubCategories { get; set; }
 }
